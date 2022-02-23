3c254e84545884f055a2bedd99e4cd694765cb57
Nathan Smith <nsmith@lcs.on.ca>
2022-02-23 10:37:44 -0500

    added a function that loads a new quote, then passed it into the 'Another One!' button and into the .task

--- Quotes/QuoteView.swift
+++ Quotes/QuoteView.swift
@@ -55,7 +55,9 @@ struct QuoteView: View {
                 
                 
                 Button(action: {
-                    
+                    Task {
+                        await loadNewQuote()
+                    }
                 }, label: {
                     Text("Another One!")
                         .font(.title2)
@@ -74,46 +76,48 @@ struct QuoteView: View {
             }
             // When the app opens, get a new joke from the web service
             .task {
-                // Assemble the URL that points to the endpoint
-                let url = URL(string: "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en")!
-                
-                // Define the type of data we want from the endpoint
-                // Configure the request to the web site
-                var request = URLRequest(url: url)
-                // Ask for JSON data
-                request.setValue("application/json",
-                                 forHTTPHeaderField: "Accept")
-                
-                // Start a session to interact (talk with) the endpoint
-                let urlSession = URLSession.shared
-                
-                // Try to fetch a new joke
-                // It might not work, so we use a do-catch block
-                do {
-                    
-                    // Get the raw data from the endpoint
-                    let (data, _) = try await urlSession.data(for: request)
-                    
-                    // Attempt to decode the raw data into a Swift structure
-                    // Takes what is in "data" and tries to put it into "currentJoke"
-                    //                                 DATA TYPE TO DECODE TO
-                    //                                         |
-                    //                                         V
-                    currentQuote = try JSONDecoder().decode(Quote.self, from: data)
-                    
-                } catch {
-                    print("Could not retrieve or decode the JSON from endpoint.")
-                    // Print the contents of the "error" constant that the do-catch block
-                    // populates
-                    print(error)
-                }
-                
+                await loadNewQuote()
             }
             .padding()
             .navigationTitle("Forismatic Quotes")
         }
     }
     
+    func loadNewQuote() async {
+        // Assemble the URL that points to the endpoint
+        let url = URL(string: "https://api.forismatic.com/api/1.0/?method=getQuote&key=457653&format=json&lang=en")!
+        
+        // Define the type of data we want from the endpoint
+        // Configure the request to the web site
+        var request = URLRequest(url: url)
+        // Ask for JSON data
+        request.setValue("application/json",
+                         forHTTPHeaderField: "Accept")
+        
+        // Start a session to interact (talk with) the endpoint
+        let urlSession = URLSession.shared
+        
+        // Try to fetch a new joke
+        // It might not work, so we use a do-catch block
+        do {
+            
+            // Get the raw data from the endpoint
+            let (data, _) = try await urlSession.data(for: request)
+            
+            // Attempt to decode the raw data into a Swift structure
+            // Takes what is in "data" and tries to put it into "currentJoke"
+            //                                 DATA TYPE TO DECODE TO
+            //                                         |
+            //                                         V
+            currentQuote = try JSONDecoder().decode(Quote.self, from: data)
+            
+        } catch {
+            print("Could not retrieve or decode the JSON from endpoint.")
+            // Print the contents of the "error" constant that the do-catch block
+            // populates
+            print(error)
+        }
+    }
 }
 
 struct QuoteView_Previews: PreviewProvider {
